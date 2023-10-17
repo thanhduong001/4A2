06995938d5dd0d069cbe4da991ef9e92a50fc6af
Thanh Duong <thanhvietduong@gmail.com>
2023-10-17 16:04:59 +0100

    read settings complete

--- Code/read_settings.f90
+++ Code/read_settings.f90
@@ -22,7 +22,11 @@
 !         nsteps
 !         ni, nj
 !     INSERT
-
+      read(5,*) av%rgas, av%gam
+      read(5,*) av%cfl, av%sfac, av%d_max
+      read(5,*) av%nsteps
+      read(5,*) av%ni, av%nj
+      
 !     Calculate other gas constants used throughout the calculation
       av%cp = av%rgas * av%gam / (av%gam - 1.0)
       av%cv = av%cp / av%gam
@@ -40,15 +44,18 @@
 !     Read the inlet boundary condition data and store into the "bcs" datatype
 !         pstag, tstag, alpha, rfin
 !     INSERT
-
+      read(5,*) bcs%pstag, bcs%tstag, bcs%alpha, bcs%rfin
+   
 !     Convert the inlet angle to radians
       bcs%alpha = bcs%alpha * 3.14159 / 180.0
 
 !     Calculate the inlet stagnation density "rostag"
 !     INSERT
+      rostag = bcs%pstag / (av%rgas * bcs%tstag)
 
 !     Read the outlet static pressure and store into the "bcs" datatype
 !     INSERT
+      read(5,*) bcs%p_out
 
 !     Print the settings to check they have been read, you can use this syntax
 !     anywhere else you want in the program to debug your code
